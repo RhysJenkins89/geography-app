31fd90f9384b48309543e53e1c835e3d38f80570
Rhys <rhys@rhysjenkins.uk>
2023-12-11 21:38:45 +0000

    Updating

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import type {PropsWithChildren} from 'react';
+import type {PropsWithChildren, ReactElement} from 'react';
 
 import {
     Button,
@@ -8,6 +8,7 @@ import {
     StatusBar,
     StyleSheet,
     Text,
+    TouchableOpacity,
     useColorScheme,
     View,
 } from 'react-native';
@@ -27,6 +28,11 @@ type SectionProps = PropsWithChildren<{
 // Generate a country from a list of countries -- probably organised into continents
 // Render this country on the app
 // Text input or just a show answer button?
+
+// Render the flag at the beginning -- not when the user guesses
+// Render the location on a map after the user presses Reveal answer
+
+// After the user has hit reveal answer, have a button that resets the state of the app
 
 type countryData = {
     country: string;
@@ -63,36 +69,58 @@ const countryDisplayData: countryData = countriesList[randomNum];
 console.log('randomNum:', randomNum);
 console.log('country:', countryDisplayData);
 
-function App(): JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
+type functionNoReturn = () => void;
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+function App(): ReactElement<countryData> {
+    const [displayAnswer, setDisplayAnswer] = useState(false);
+    const [countryData, setCountryData] = useState<countryData>(
+        countriesList[randomNum],
+    );
+
+    const randonNumberToReturn: () => number = () => {
+        return Math.floor(Math.random() * countriesList.length);
     };
 
-    const [displayAnswer, setDisplayAnswer] = useState(false);
-
-    const handlePress = () => {
+    const handlePress: functionNoReturn = () => {
         console.log('handle press');
         setDisplayAnswer(true);
+    };
+
+    const newCountry: functionNoReturn = () => {
+        // Reset the display answer button
+        // Reset the next country button
+        // Render a new country to the screen
+        // If this were a browser, I might just reset the page
+        setDisplayAnswer(false);
+        // Reset the data in countryDisplayData
+        setCountryData(countriesList[randonNumberToReturn()]);
     };
 
     return (
         <SafeAreaView>
             <ScrollView>
                 <View>
-                    <Text>
-                        What is the capital of {countryDisplayData.country}?
-                    </Text>
-                    <Button
-                        title="Reveal answer"
-                        onPress={() => handlePress()}></Button>
+                    <Text>What is the capital of {countryData.country}?</Text>
+                    <TouchableOpacity
+                        style={
+                            displayAnswer ? styles.displayNone : styles.display
+                        }
+                        onPress={() => handlePress()}>
+                        <Text>Reveal answer</Text>
+                    </TouchableOpacity>
                     <Text
                         style={
                             displayAnswer ? styles.display : styles.displayNone
                         }>
                         {countryDisplayData.capital}
                     </Text>
+                    <TouchableOpacity
+                        style={
+                            displayAnswer ? styles.display : styles.displayNone
+                        }
+                        onPress={() => newCountry()}>
+                        <Text>Next country</Text>
+                    </TouchableOpacity>
                 </View>
             </ScrollView>
         </SafeAreaView>
