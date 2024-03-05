dcae51b7de00a4ef097fe15003af9a4b543474ed
Rhys <rhys@rhysjenkins.uk>
2024-03-05 21:30:44 +0000

    Calling auth breaks the app

--- login/Login.tsx
+++ login/Login.tsx
@@ -6,69 +6,40 @@ import {
     TouchableOpacity,
     StyleSheet,
 } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const LoginScreen: React.FC = () => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
     const handleLogin = () => {
-        // Here you can implement your login logic, such as API calls, authentication, etc.
         console.log('Username:', username);
         console.log('Password:', password);
+        auth().createUserWithEmailAndPassword(username, password);
+        setUsername('');
+        setPassword('');
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.label}>Username:</Text>
+        <View>
+            <Text>Username:</Text>
             <TextInput
-                style={styles.input}
                 onChangeText={text => setUsername(text)}
                 value={username}
                 placeholder="Enter your username"
             />
-            <Text style={styles.label}>Password:</Text>
+            <Text>Password:</Text>
             <TextInput
-                style={styles.input}
                 onChangeText={text => setPassword(text)}
                 value={password}
                 placeholder="Enter your password"
                 secureTextEntry
             />
-            <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                <Text style={styles.buttonText}>Login</Text>
+            <TouchableOpacity onPress={handleLogin}>
+                <Text>Login</Text>
             </TouchableOpacity>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    label: {
-        fontSize: 18,
-        marginBottom: 5,
-    },
-    input: {
-        width: '80%',
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        borderRadius: 5,
-        marginBottom: 20,
-        paddingLeft: 10,
-    },
-    button: {
-        backgroundColor: 'blue',
-        padding: 10,
-        borderRadius: 5,
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 18,
-    },
-});
 
 export default LoginScreen;
